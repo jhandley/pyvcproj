1c1d8b45f4819437290bd499168d9846fd6868de
Josh Handley <jhandley@lkc.com>
2014-10-05 17:56:27 -0400

    Get additional link dependencies, additional includes from project

--- vcproj/project.py
+++ vcproj/project.py
@@ -1,26 +1,56 @@
 import xml.etree.ElementTree as ET
+import re
+
+_MS_BUILD_NAMESPACE = "http://schemas.microsoft.com/developer/msbuild/2003"
+_REGEX_CONFIG_CONDITION = re.compile(r"""'\$\(Configuration\)\|\$\(Platform\)'=='(\w+)\|(\w+)'""")
+
+def _parse_config_condition(condition):
+    return _REGEX_CONFIG_CONDITION.match(condition).groups()
+
+def _matches_platform_configuration(item_group, platform, configuration):
+    (p, c) = _parse_config_condition(item_group.attrib['Condition'])
+    return (p == "All Configurations" or p == platform) and (c == "All Configurations" or c == configuration)
 
 class Project(object):
     """Visual C++ project file (.vcxproj)."""
 
-    __MS_BUILD_NAMESPACE = "http://schemas.microsoft.com/developer/msbuild/2003"
-
     def __init__(self, filename):
         """Create a Project instance for project file *filename*."""
-        ET.register_namespace('', self.__MS_BUILD_NAMESPACE)
+        ET.register_namespace('', _MS_BUILD_NAMESPACE)
         self.filename = filename
         self.xml = ET.parse(filename)
 
     def source_files(self):
         """List source files in project."""
-        return [c.attrib['Include'] for c in self.xml.findall(".//{" + self.__MS_BUILD_NAMESPACE + "}ClCompile[@Include]")]
+        return [c.attrib['Include'] for c in self.xml.findall(".//{" + _MS_BUILD_NAMESPACE + "}ClCompile[@Include]")]
 
     def include_files(self):
         """List include files in project."""
-        return [c.attrib['Include'] for c in self.xml.findall(".//{" + self.__MS_BUILD_NAMESPACE + "}ClInclude[@Include]")]
+        return [c.attrib['Include'] for c in self.xml.findall(".//{" + _MS_BUILD_NAMESPACE + "}ClInclude[@Include]")]
+    
+    def __item_groups_for_config(self, platform, configuration):
+        groups = self.xml.findall("./{" + _MS_BUILD_NAMESPACE + "}ItemDefinitionGroup")
+        return filter(lambda g: _matches_platform_configuration(g, platform, configuration), groups)
+
+    def __item_group_item_for_config(self, platform, configuration, subgroup_name, item_name):
+        groups = self.__item_groups_for_config(platform, configuration)
+        if len(groups) == 0:
+            return []
+        item = groups[0].find("{" + _MS_BUILD_NAMESPACE + "}" + subgroup_name + "/{" + _MS_BUILD_NAMESPACE + "}" + item_name)
+        if item is None:
+            return []
+        return item.text.split(';')
+        
+    def additional_link_dependencies(self, platform, configuration):
+        return self.__item_group_item_for_config(platform, configuration, "Link", "AdditionalDependencies")
+        
+    def additional_include_directories(self, platform, configuration):
+        return self.__item_group_item_for_config(platform, configuration, "ClCompile", "AdditionalIncludeDirectories")
 
     def set_additional_include_directories(self, additional_includes):
-        include_nodes = self.xml.findall("./{" + self.__MS_BUILD_NAMESPACE + "}ItemDefinitionGroup/{" + self.__MS_BUILD_NAMESPACE + "}ClCompile/{" + __MS_BUILD_NAMESPACE + "}AdditionalIncludeDirectories")
+        include_nodes = self.xml.findall("./{" + self.__MS_BUILD_NAMESPACE + "}ItemDefinitionGroup/{" +
+                                         self.__MS_BUILD_NAMESPACE + "}ClCompile/{" +
+                                         self.__MS_BUILD_NAMESPACE + "}AdditionalIncludeDirectories")
         for n in include_nodes:
           n.text = additional_includes
 
--- vcproj/tests/test_project.py
+++ vcproj/tests/test_project.py
@@ -7,3 +7,17 @@ def test_source_files():
 def test_include_files():
     p = vcproj.project.parse('vcproj/tests/test_solution/test/test.vcxproj')
     assert(list(p.include_files()) == ['Resource.h', 'stdafx.h', 'targetver.h', 'test.h'])
+
+def test_additional_link_dependencies():
+    p = vcproj.project.parse('vcproj/tests/test_solution/test/test.vcxproj')
+    assert(p.additional_link_dependencies('Debug', 'Win32') ==
+           ['somethingextra.lib', 'kernel32.lib', 'user32.lib', 'gdi32.lib', 'winspool.lib', 'comdlg32.lib', 'advapi32.lib', 'shell32.lib', 'ole32.lib', 'oleaut32.lib', 'uuid.lib', 'odbc32.lib', 'odbccp32.lib', '%(AdditionalDependencies)'])
+    assert(p.additional_link_dependencies('Release', 'Win32') ==
+           ['somethingextra.lib', 'somethingelse.lib', 'kernel32.lib', 'user32.lib', 'gdi32.lib', 'winspool.lib', 'comdlg32.lib', 'advapi32.lib', 'shell32.lib', 'ole32.lib', 'oleaut32.lib', 'uuid.lib', 'odbc32.lib', 'odbccp32.lib', '%(AdditionalDependencies)'])
+    assert(p.additional_link_dependencies('Foo', 'Win32') == [])
+
+def test_additional_include_directories():
+    p = vcproj.project.parse('vcproj/tests/test_solution/test/test.vcxproj')
+    assert(p.additional_include_directories('Debug', 'Win32') == ['..', 'foo/bar'])
+    assert(p.additional_include_directories('Release', 'Win32') == [])
+    assert(p.additional_include_directories('Foo', 'Win32') == [])
