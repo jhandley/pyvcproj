74b2a3c3cb05289a630d2477e49cfa0db9b910bf
Josh Handley <jhandley@lkc.com>
2014-10-11 19:21:44 -0400

    Get/set project dependencies in solution

--- vcproj/solution.py
+++ vcproj/solution.py
@@ -1,22 +1,118 @@
-import re
+import re, codecs
+
+class SolutionFileError(Exception):
+    pass
+
+_REGEX_PROJECT_FILE = re.compile(r"""\s*Project\("(\{[A-Za-z0-9-]+\})"\) = "(\w+)", "((?:(?:\w+)\\)*(?:\w+)\.vcxproj)", \"({[A-Za-z0-9-]+})\"""")
+_REGEX_END_PROJECT = re.compile(r"""\s*EndProject""")
+_REGEX_PROJECT_DEPENDENCIES_SECTION = re.compile(r"""\s*ProjectSection\((\w+)\) = postProject""")
+_REGEX_END_PROJECT_SECTION = re.compile(r"""\s*EndProjectSection""")
+_REGEX_DEPENDENCY = re.compile(r"""\s*(\{[A-Za-z0-9-]+\})\s*=\s*(\{[A-Za-z0-9-]+\})""")
 
 class Solution(object):
     """Visual C++ solution file (.sln)."""
 
-    __REGEX_PROJECT_FILE = re.compile(r"""Project\("\{[A-Za-z0-9-]+\}"\) = "\w+", "(((\w+)\\)*(\w+)\.vcxproj)", \"{[A-Za-z0-9-]+}\"""")
-
     def __init__(self, filename):
         """Create a Solution instance for solution file *name*."""
         self.filename = filename
-        with open(self.filename) as f:
-            self.lines = f.readlines()        
+        self.projects = []
+        with open(self.filename, 'rb') as f:
+            line = f.readline()
+            while line:
+                line = f.readline()
+                if line.startswith("Project"):
+                    match = _REGEX_PROJECT_FILE.match(line)
+                    if match:
+                        self.projects.append(Solution.__read_project(match.groups(), f))
+                elif line.startswith("Global"):
+                    self.globals = Solution.__read_global(f);
 
-    def projects(self):
-        """List project files (.vcxproj.) in solution."""
-        for line in self.lines:
-            match = self.__REGEX_PROJECT_FILE.match(line)
+    @staticmethod
+    def __read_project(project, f):
+        dependencies = []
+        while True:                          
+            line = f.readline()
+            if line is None:
+                raise SolutionFileError("Missing end project")
+            if _REGEX_END_PROJECT.match(line):
+                break
+            if _REGEX_PROJECT_DEPENDENCIES_SECTION.match(line):
+                dependencies = Solution.__read_dependencies(f)
+        return project + (dependencies,)
+    
+    @staticmethod
+    def __read_dependencies(f):
+        dependencies = []
+        while True:                          
+            line = f.readline()
+            if line is None:
+                raise SolutionFileError("Missing end dependencies section")
+            if _REGEX_END_PROJECT_SECTION.match(line):
+                break
+            match = _REGEX_DEPENDENCY.match(line)
             if match:
-                yield match.group(1)
+                dependencies.append(match.group(1))
+        return dependencies
+
+    @staticmethod
+    def __read_global(f):
+        result = ""
+        while True:                          
+            line = f.readline()
+            if line is None:
+                raise SolutionFileError("Missing end global")
+            if line.startswith("EndGlobal"):
+                break
+            result += line
+        return result
+    
+    def project_files(self):
+        """List project files (.vcxproj.) in solution."""
+        return map(lambda p: p[2], self.projects)
+
+    def project_names(self):
+        """List project files (.vcxproj.) in solution."""
+        return map(lambda p: p[1], self.projects)
+
+    def dependencies(self, project_name):
+        """List names of projects dependent on project *project_name*"""
+        project = self.__project_from_name(project_name)
+        if not project:
+            raise SolutionError("Can't find project with name " + project_name)
+        return map(lambda d: self.__project_from_id(d)[1], project[4])
+
+    def set_dependencies(self, project_name, dependencies):
+        """Set names of projects dependent on project *project_name* to *dependencies*"""
+        project = self.__project_from_name(project_name)
+        if not project:
+            raise SolutionError("Can't find project with name " + project_name)
+        index = self.projects.index(project)
+        self.projects[index] = project[0:4] + (map(lambda d: self.__project_from_name(d)[3], dependencies),)
+    
+    def __project_from_name(self, project_name):
+        return next((p for p in self.projects if p[1] == project_name), None)
+
+    def __project_from_id(self, project_id):
+        return next((p for p in self.projects if p[3] == project_id), None)
+
+    def write(self, filename = None):
+        """Save solution file."""
+        filename = filename or self.filename
+        with codecs.open(filename, "w", "utf-8-sig") as f:
+            f.write("\r\nMicrosoft Visual Studio Solution File, Format Version 11.00\r\n")
+            f.write("# Visual Studio 2010\r\n")
+            for project in self.projects:
+                f.write("Project(\"{0}\") = \"{1}\", \"{2}\", \"{3}\"\r\n".format(*project[0:4]))
+                dependencies = project[4]
+                if dependencies:
+                    f.write("\tProjectSection(ProjectDependencies) = postProject\r\n")
+                    for d in dependencies:
+                        f.write("\t\t{0} = {0}\r\n".format(d))
+                    f.write("\tEndProjectSection\r\n")
+                f.write("EndProject\r\n")
+            f.write("Global\r\n")
+            f.write(self.globals)
+            f.write("EndGlobal\r\n")
 
 def parse(filename):
     """Parse solution file *filename* and return Solution instance."""
--- vcproj/tests/test_solution.py
+++ vcproj/tests/test_solution.py
@@ -1,7 +1,28 @@
 import vcproj.solution
+import tempfile, filecmp
 
-def test_projects():
+def test_project_names():
     s = vcproj.solution.parse('vcproj/tests/test_solution/test.sln')
-    assert list(s.projects()) == ['test\\test.vcxproj', 'lib1\\lib1.vcxproj', 'lib2\\lib2.vcxproj']
-    
+    assert list(s.project_names()) == ['test', 'lib1', 'lib2']
 
+def test_project_files():
+    s = vcproj.solution.parse('vcproj/tests/test_solution/test.sln')
+    assert list(s.project_files()) == ['test\\test.vcxproj', 'lib1\\lib1.vcxproj', 'lib2\\lib2.vcxproj']
+    
+def test_dependencies():
+    s = vcproj.solution.parse('vcproj/tests/test_solution/test.sln')
+    assert list(s.dependencies('test')) == ['lib1', 'lib2']
+    
+def test_set_dependencies():
+    s = vcproj.solution.parse('vcproj/tests/test_solution/test.sln')
+    s.set_dependencies('lib1', ['lib2'])
+    assert list(s.dependencies('lib1')) == ['lib2']
+
+def test_write():
+    s = vcproj.solution.parse('vcproj/tests/test_solution/test.sln')
+    temp = tempfile.NamedTemporaryFile()
+    temp.close()
+    s.write(temp.name)
+    assert filecmp.cmp('vcproj/tests/test_solution/test.sln', temp.name)
+    
+    
