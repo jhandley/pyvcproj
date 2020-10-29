3c48fbb4d7b10230fec0e987fd6fe122ec2f1dc6
Victor Uriarte <victor.m.uriarte@intel.com>
2020-10-29 11:36:09 -0700

    Match list_source_files example format to others

--- examples/list_source_files.py
+++ examples/list_source_files.py
@@ -3,16 +3,27 @@
 # List all source and include files for each project in a solution.
 
 import os
+import sys
 
 import vcproj.project
 import vcproj.solution
 
-solution_file = '../vcproj/tests/test_solution/test.sln'
-solution = vcproj.solution.parse(solution_file)
-for project_file in solution.project_files():
-    print(project_file)
-    project = vcproj.project.parse(os.path.join(os.path.dirname(solution_file), project_file))
-    for source_file in project.source_files():
-        print("\t" + source_file)
-    for include_file in project.include_files():
-        print("\t" + include_file)
+
+def main(argv):
+    if len(argv) < 2:
+        print("Usage: " + argv[0] + " <solution file>")
+        sys.exit(2)
+    solution_file = argv[1]
+    solution = vcproj.solution.parse(solution_file)
+    for project_file in solution.project_files():
+        print(project_file)
+        project = vcproj.project.parse(os.path.join(os.path.dirname(solution_file), project_file))
+        for source_file in project.source_files():
+            print("\t" + source_file)
+        for include_file in project.include_files():
+            print("\t" + include_file)
+
+
+if __name__ == '__main__':
+    # main(sys.argv)
+    main(['', '../vcproj/tests/test_solution/test.sln'])
